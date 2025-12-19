88202505705ec76592a151098819a28d94cf52f0
divyanshutn74 <divyanshutandan74@gmail.com>
2025-12-20 01:20:54 +0530

    FIRST COMMIT

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import { connectDB } from './config/db.js'; // .js is necessary in db.js
+import cors from 'cors'
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 connectDB();
 
+app.use(cors({
+    origin: "*",
+    credentials: true
+}))
 app.use(express.json()); // This parse every req.body from every page
 
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.use('/api/users', authRoutes);
 
