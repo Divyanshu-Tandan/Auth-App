5d70ca725956ce1e8a594e28536e952907b0c0ce
divyanshutn74 <divyanshutandan74@gmail.com>
2026-06-07 17:48:03 +0530

    Deployment Error Fixed

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import User from '../models/User.js';
+import User from '../models/user.js';
 import protect from '../middleware/auth.middleware.js';
 import verifyAdmin from '../middleware/admin.middleware.js'
 import jwt from 'jsonwebtoken';
