117148485d657cd597e38486a32c3752baf49291
divyanshutn74 <divyanshutandan74@gmail.com>
2026-06-13 00:37:56 +0530

    Debugging

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,10 @@ export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.URI);
         console.log(`MongoDB Connected ${conn.connection.host}`);
-    } 
-    catch(error) {
+        console.log(process.env.EMAIL_PASS);
+        console.log(process.env.EMAIL_USER);
+    }
+    catch (error) {
         console.log(error);
         process.exit(1);
     }
