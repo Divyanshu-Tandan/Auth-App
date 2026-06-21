39b4563ac5d5d5acacfbc0935804d658b372c6ec
divyanshutn74 <divyanshutandan74@gmail.com>
2026-06-21 12:37:24 +0530

    Final Fix

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,6 @@ app.use(cors({
 app.use(express.json()); // This parse every req.body from every page
 app.use(helmet()); // Helmet helps secure your Express apps by setting various HTTP headers
 app.use(cookieParser()); // This parse every cookie from every page
-
 app.use('/api/users', authRoutes); // this is the route for authentication
 
 app.get('/health', (req, res) => {
